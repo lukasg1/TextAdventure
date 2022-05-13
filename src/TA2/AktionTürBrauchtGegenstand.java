9a2b36a04b1d555968b6bd8604358a90c2d6c080
Burbielmon <simon@burbiel.de>
2022-05-13 17:17:38 +0200

    Update

--- src/TA2/AktionTürBrauchtGegenstand.java
+++ src/TA2/AktionTürBrauchtGegenstand.java
@@ -1,26 +1,27 @@
 package TA2;
 
+import static TA2.Main.gv;
 import static TA2.Main.rv;
 
 public class AktionTürBrauchtGegenstand extends AktionBrauchtRaum{
     protected Raum zielraum;
-    protected VerwaltungGegenstände gegenstandverwaltung;
+
     protected Gegenstand gegenstand;
 
 
-    public AktionTürBrauchtGegenstand(String aktionsname, boolean aktiv, boolean sichtbar, String beschreibung, String ausführungsText, Raum raum,Raum zielraum,Gegenstand gegenstand,VerwaltungGegenstände gegentandverwaltung) {
+    public AktionTürBrauchtGegenstand(String aktionsname, boolean aktiv, boolean sichtbar, String beschreibung, String ausführungsText, Raum raum,Raum zielraum,Gegenstand gegenstand) {
         super(aktionsname, aktiv, sichtbar, beschreibung, ausführungsText, raum);
         this.zielraum=zielraum;
         this.aktionsname= raum.getRaumName()+zielraum.getRaumName()+"Tür";
         this.gegenstand=gegenstand;
-        this.gegenstandverwaltung=gegentandverwaltung;
+
 
     }
 
     @Override
     public void ausführen() {
 
-            if(gegenstandverwaltung.sucheGegnstandImInventar(gegenstand)==gegenstand){
+            if(gv.sucheGegnstandImInventar(gegenstand)==gegenstand){
                 System.out.println(ausführungsText);
                 rv.setAktuellerRaum(zielraum);
                 this.beschreibung= "Von "+raum.getRaumName()+" nach "+zielraum.getRaumName()+" gehen";
