ebe7e10b192ba3ca6a9e0bd9f410b6968fc20c3f
Burbielmon <simon@burbiel.de>
2022-05-13 15:55:15 +0200

    Update

--- src/TA2/AktionTür.java
+++ src/TA2/AktionTür.java
@@ -1,12 +1,37 @@
 package TA2;
 
+import static TA2.Main.rv;
+
 public class AktionTür extends AktionBrauchtRaum{
-    public AktionTür(String aktionsname, boolean aktiv, boolean sichtbar, String beschreibung, String ausführungsText, Raum raum) {
+    protected Raum zielraum;
+    protected boolean verschlossen;
+    protected String verschlossenText;
+    public AktionTür(String aktionsname, boolean aktiv, boolean sichtbar, String beschreibung, String ausführungsText, Raum raum,Raum Zielraum) {
         super(aktionsname, aktiv, sichtbar, beschreibung, ausführungsText, raum);
+        this.zielraum=zielraum;
+        this.verschlossen=verschlossen;
+        this.aktionsname= raum.getRaumName()+zielraum.getRaumName()+"Tür";
+
+        this.verschlossenText="Die Türe geht nicht auf. Die Türe muss verschlossen oder versperrt sein.";
     }
 
     @Override
     public void ausführen() {
+        Raum raumSpeicher= raum;
+        if(raum==raum){
+            raumSpeicher=zielraum;
+        }
+
+        if(verschlossen==false){
+            System.out.println(ausführungsText);
+            rv.setAktuellerRaum(raum);
+            this.beschreibung= "Von "+raumSpeicher.getRaumName()+" nach "+raum.getRaumName()+" gehen";
+            this.ausführungsText="";
+        }
+        else
+        {
+            System.out.println(verschlossenText);
+        }
 
     }
 }
