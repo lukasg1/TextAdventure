a278efd7e2970e462454b7571765b3f5b764d751
Burbielmon <simon@burbiel.de>
2022-05-13 17:53:06 +0200

    Update

--- src/TA2/AktionTür.java
+++ src/TA2/AktionTür.java
@@ -2,42 +2,41 @@ package TA2;
 
 import static TA2.Main.rv;
 
-public class AktionTür extends AktionBrauchtRaum{
+public class AktionTür extends AktionBrauchtRaum {
     protected Raum zielraum;
 
-    public AktionTür(String aktionsname, boolean aktiv, boolean sichtbar, String beschreibung, String ausführungsText, Raum raum,Raum zielraum) {
+    public AktionTür(String aktionsname, boolean aktiv, boolean sichtbar, String beschreibung, String ausführungsText, Raum raum, Raum zielraum) {
         super(aktionsname, aktiv, sichtbar, beschreibung, ausführungsText, raum);
-        this.zielraum=zielraum;
-        this.aktionsname= raum.getRaumName()+zielraum.getRaumName()+"Tür";
+        this.zielraum = zielraum;
+        this.aktionsname = raum.getRaumName() + zielraum.getRaumName() + "Tür";
 
     }
 
     public void update() {
-        if(sichtbar==true&&aktiv==true&&raum==rv.getAktuellerRaum()&&sonderdeaktivierung==false){
-            verfügbar=true;
+        if (sichtbar == true && aktiv == true && raum == rv.getAktuellerRaum() && sonderdeaktivierung == false) {
+            verfügbar = true;
             this.beschreibung = "Von " + raum.getRaumName() + " nach " + zielraum.getRaumName() + " gehen";
-        }else if(sichtbar==true&&aktiv==true&&zielraum==rv.getAktuellerRaum()&&sonderdeaktivierung==false){
-            verfügbar=true;
+        } else if (sichtbar == true && aktiv == true && zielraum == rv.getAktuellerRaum() && sonderdeaktivierung == false) {
+            verfügbar = true;
             this.beschreibung = "Von " + zielraum.getRaumName() + " nach " + raum.getRaumName() + " gehen";
-        }
-        else verfügbar=false;
+        } else verfügbar = false;
 
     }
 
     @Override
     public void ausführen() {
-        if(raum==rv.getAktuellerRaum()) {
+        if (raum == rv.getAktuellerRaum()) {
             System.out.println(ausführungsText);
             rv.setAktuellerRaum(zielraum);
 
             this.ausführungsText = "";
-        }else if(zielraum==rv.getAktuellerRaum()) {
+        } else if (zielraum == rv.getAktuellerRaum()) {
             System.out.println(ausführungsText);
             rv.setAktuellerRaum(raum);
 
             this.ausführungsText = "";
 
 
-
+        }
     }
 }
