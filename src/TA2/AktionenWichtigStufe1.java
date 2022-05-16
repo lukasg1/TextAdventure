1549a33b6deca54c253777f10c31c62d4c949f14
simon <simon@burbiel.de>
2022-05-16 10:09:11 +0200

    Update klein

---
+++ src/TA2/AktionenWichtigStufe1.java
@@ -0,0 +1,17 @@
+package TA2;
+
+abstract class AktionenWichtigStufe1 extends AktionOberklasse{
+    public AktionenWichtigStufe1() {
+        this.wichtig=true;
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void ausführen() {
+
+    }
+}
