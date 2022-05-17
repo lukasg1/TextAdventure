7043d5bb8e8822648ed96cfd3540bb8bb2665297
timkeicher <tim-keicher@web.de>
2022-05-17 11:35:05 +0200

    Tim Keicher update Aktionsverwaltung

--- src/TA2/Auswahl.java
+++ src/TA2/Auswahl.java
@@ -12,8 +12,143 @@ public class Auswahl {
     private JButton aktion1Button;
     private JButton aktion2Button;
     private JButton aktion4Button;
-    private JButton button1;
-    private JButton button2;
+    private JButton aktion6Button;
+    private JButton aktion7Button;
+    private JButton aktion8Button;
+    private JButton aktion9Button;
+    private JButton aktion10Button;
+
+    public void buttonbeschreibung(){
+        av.update();
+        int anzahl=av.anzahlaktiveaktionen();
+        if(anzahl>10){
+            anzahl=10;
+        }
+        switch (anzahl){
+            case 1:
+                aktion1Button.setText(av.aktionen[0].getBeschreibung());
+                aktion2Button.hide();
+                aktion3Button.hide();
+                aktion4Button.hide();
+                aktion5Button.hide();
+                aktion6Button.hide();
+                aktion7Button.hide();
+                aktion8Button.hide();
+                aktion9Button.hide();
+                aktion10Button.hide();
+
+            case 2:
+                aktion1Button.setText(av.aktionen[0].getBeschreibung());
+                aktion2Button.setText(av.aktionen[1].getBeschreibung());
+                aktion3Button.hide();
+                aktion4Button.hide();
+                aktion5Button.hide();
+                aktion6Button.hide();
+                aktion6Button.hide();
+                aktion7Button.hide();
+                aktion8Button.hide();
+                aktion9Button.hide();
+                aktion10Button.hide();
+
+            case 3:
+                aktion1Button.setText(av.aktionen[0].getBeschreibung());
+                aktion2Button.setText(av.aktionen[1].getBeschreibung());
+                aktion3Button.setText(av.aktionen[2].getBeschreibung());
+                aktion4Button.hide();
+                aktion5Button.hide();
+                aktion6Button.hide();
+                aktion7Button.hide();
+                aktion8Button.hide();
+                aktion9Button.hide();
+                aktion10Button.hide();
+
+            case 4:
+                aktion1Button.setText(av.aktionen[0].getBeschreibung());
+                aktion2Button.setText(av.aktionen[1].getBeschreibung());
+                aktion3Button.setText(av.aktionen[2].getBeschreibung());
+                aktion4Button.setText(av.aktionen[3].getBeschreibung());
+                aktion5Button.hide();
+                aktion6Button.hide();
+                aktion7Button.hide();
+                aktion8Button.hide();
+                aktion9Button.hide();
+                aktion10Button.hide();
+
+            case 5:
+                aktion1Button.setText(av.aktionen[0].getBeschreibung());
+                aktion2Button.setText(av.aktionen[1].getBeschreibung());
+                aktion3Button.setText(av.aktionen[2].getBeschreibung());
+                aktion4Button.setText(av.aktionen[3].getBeschreibung());
+                aktion5Button.setText(av.aktionen[4].getBeschreibung());
+                aktion6Button.hide();
+                aktion7Button.hide();
+                aktion8Button.hide();
+                aktion9Button.hide();
+                aktion10Button.hide();
+
+            case 6:
+                aktion1Button.setText(av.aktionen[0].getBeschreibung());
+                aktion2Button.setText(av.aktionen[1].getBeschreibung());
+                aktion3Button.setText(av.aktionen[2].getBeschreibung());
+                aktion4Button.setText(av.aktionen[3].getBeschreibung());
+                aktion5Button.setText(av.aktionen[4].getBeschreibung());
+                aktion6Button.setText(av.aktionen[5].getBeschreibung());
+                aktion7Button.hide();
+                aktion8Button.hide();
+                aktion8Button.hide();
+                aktion9Button.hide();
+                aktion10Button.hide();
+
+            case 7:
+                aktion1Button.setText(av.aktionen[0].getBeschreibung());
+                aktion2Button.setText(av.aktionen[1].getBeschreibung());
+                aktion3Button.setText(av.aktionen[2].getBeschreibung());
+                aktion4Button.setText(av.aktionen[3].getBeschreibung());
+                aktion5Button.setText(av.aktionen[4].getBeschreibung());
+                aktion6Button.setText(av.aktionen[5].getBeschreibung());
+                aktion7Button.setText(av.aktionen[6].getBeschreibung());
+                aktion8Button.hide();
+                aktion9Button.hide();
+                aktion10Button.hide();
+
+            case 8:
+                aktion1Button.setText(av.aktionen[0].getBeschreibung());
+                aktion2Button.setText(av.aktionen[1].getBeschreibung());
+                aktion3Button.setText(av.aktionen[2].getBeschreibung());
+                aktion4Button.setText(av.aktionen[3].getBeschreibung());
+                aktion5Button.setText(av.aktionen[4].getBeschreibung());
+                aktion6Button.setText(av.aktionen[5].getBeschreibung());
+                aktion7Button.setText(av.aktionen[6].getBeschreibung());
+                aktion8Button.setText(av.aktionen[7].getBeschreibung());
+                aktion9Button.hide();
+                aktion10Button.hide();
+
+            case 9:
+                aktion1Button.setText(av.aktionen[0].getBeschreibung());
+                aktion2Button.setText(av.aktionen[1].getBeschreibung());
+                aktion3Button.setText(av.aktionen[2].getBeschreibung());
+                aktion4Button.setText(av.aktionen[3].getBeschreibung());
+                aktion5Button.setText(av.aktionen[4].getBeschreibung());
+                aktion6Button.setText(av.aktionen[5].getBeschreibung());
+                aktion7Button.setText(av.aktionen[6].getBeschreibung());
+                aktion8Button.setText(av.aktionen[7].getBeschreibung());
+                aktion9Button.setText(av.aktionen[8].getBeschreibung());
+                aktion10Button.hide();
+
+            case 10:
+                aktion1Button.setText(av.aktionen[0].getBeschreibung());
+                aktion2Button.setText(av.aktionen[1].getBeschreibung());
+                aktion3Button.setText(av.aktionen[2].getBeschreibung());
+                aktion4Button.setText(av.aktionen[3].getBeschreibung());
+                aktion5Button.setText(av.aktionen[4].getBeschreibung());
+                aktion6Button.setText(av.aktionen[5].getBeschreibung());
+                aktion7Button.setText(av.aktionen[6].getBeschreibung());
+                aktion8Button.setText(av.aktionen[7].getBeschreibung());
+                aktion9Button.setText(av.aktionen[8].getBeschreibung());
+                aktion10Button.setText(av.aktionen[9].getBeschreibung());
+
+        }
+    }
 
 
     public Auswahl() {
