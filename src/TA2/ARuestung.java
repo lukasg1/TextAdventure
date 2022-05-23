dd7bc4f65fd4984a0029c4305bfef80860bb56ba
Samuel <samuelheinrich2002@gmail.com>
2022-05-23 20:41:53 +0200

    kleines update samuel

--- src/TA2/ARuestung.java
+++ src/TA2/ARuestung.java
@@ -2,7 +2,7 @@ package TA2;
 
 import static TA2.Main.*;
 
-public class ARuestung extends AktionOberklasse{
+public class ARuestung extends AktionOberklasse {
 
     protected Raum raum;
     protected AktionOberklasse aktion;
@@ -16,7 +16,7 @@ public class ARuestung extends AktionOberklasse{
         this.aktiv = true;
         this.sichtbar = true;
         this.wichtig = false;
-        this.beschreibung="Ritterrüstungen begutachten";
+        this.beschreibung = "Ritterrüstungen begutachten";
         this.zaehler = 0;
 
 
@@ -24,10 +24,9 @@ public class ARuestung extends AktionOberklasse{
 
     @Override
     public void update() {
-        if(sichtbar==true&&aktiv==true&&raum==rv.getAktuellerRaum()&&sonderdeaktivierung==false){
-            verfügbar=true;
-        }
-        else verfügbar=false;
+        if (sichtbar == true && aktiv == true && raum == rv.getAktuellerRaum() && sonderdeaktivierung == false) {
+            verfügbar = true;
+        } else verfügbar = false;
 
     }
 
@@ -42,18 +41,19 @@ public class ARuestung extends AktionOberklasse{
                 if (gv.sucheGegnstandzumZerstören() != null) {
                     this.ausführungsText = "Du greifst die Rüstung an und zerschmetterst sie mit " + gv.sucheGegnstandzumZerstören().getName() + ".Du hast den Rüstungsgeist besiegt. Dabei geht jedoch " + gv.sucheGegnstandzumZerstören().getName() + "kaputt.";
                     gv.sucheGegnstandzumZerstören().deaktivieren();
+                    gv.sucheGegenstand(schwert).setSichtbar(true);
                     zaehler = 2;
 
                 } else {
-                    if(w100.würfel()>50){
+                    if (w100.würfel() > 50) {
                         this.ausführungsText = "Der Engel Ron rettet dich vor einem tödlichen Schlag. Benebelt wachst du auf dem Flur vor dem Arenaraum auf. ";
-                        zaehler=2;
+                        zaehler = 2;
 
+                    } else {
+                        this.ausführungsText = "Ohne Waffen kannst du dich nicht verteidigen. Die Ritterrüstung schwingt ihr Schwert, trifft dich und dir wird schwarz vor Augen. ";
+                        zaehler = 1;
                     }
-                    else{
-                    this.ausführungsText = "Ohne Waffen kannst du dich nicht verteidigen. Die Ritterrüstung schwingt ihr Schwert, trifft dich und dir wird schwarz vor Augen. ";
-                    zaehler = 1;
-                }}
+                }
 
 
                 this.beschreibung = "Greife die Rüstung an";
